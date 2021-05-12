e354bd1d4bdf8b7857dc94006383d54195a010df
DocenkoG <docn@mail.ru>
2021-05-12 16:26:01 +0300

    Увеличен код товара путем добавления в него серии и подгруппы.

--- main.py
+++ main.py
@@ -177,7 +177,7 @@ def convert_excel2csv(cfg):
                     shablon = str(round(vvv1 * vvv2, 2))
                 recOut[outColName] = shablon.strip()
 
-            recOut['код'] = nameToId(recOut['код'])
+            recOut['код'] = nameToId(recOut['код'] + series + subgrp)
             '''
             if  recOut['продажа'] == '0.1':
                 recOut['валюта'] = 'USD'
